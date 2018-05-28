c51236d0fec60e4e93d5ea75b486251a0a7767d2
Yann Bacquet <yann.bacquet@bureauveritas.com>
2018-05-28 18:00:50 +0200

    Skynet avec boucle recursive

--- CodingGame1/SkyNet.cs
+++ CodingGame1/SkyNet.cs
@@ -6,16 +6,23 @@ namespace IAGames
 {
     class SkyNet
     {
+        static int L;
+        static int E;
+        private static int[,] links;
+        private static int[] gates;
+
+       static  List<Tuple<int,int>> lstNoeuds =new List<Tuple<int, int>>();
+
         static void Main(string[] args)
         {
             string[] inputs;
             inputs = Console.ReadLine().Split(' ');
             int N = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
-            int L = int.Parse(inputs[1]); // the number of links
-            int E = int.Parse(inputs[2]); // the number of exit gateways
+             L = int.Parse(inputs[1]); // the number of links
+             E = int.Parse(inputs[2]); // the number of exit gateways
 
-            int[,] links = new int[L, 3]; // list de liens composé de 2 noeuds + isOpen
-            int[] gates = new int[E]; //les noeuds gates
+            links = new int[L, 3]; // list de liens composé de 2 noeuds + isOpen
+            gates = new int[E]; //les noeuds gates
 
             for (int i = 0; i < L; i++)
             {
@@ -44,17 +51,18 @@ namespace IAGames
 
                 bool hasClosed = false;
 
-               
-                for (int i = 0; i < E; i++)
+                //Si va gagner  : Skynet sur lien avec 1 gate
+                for (int i = 0; i < E; i++) //pour toutes les gates
                 {
                     int gate = gates[i];
-                    for (int j = 0; j < L; j++)
+                    for (int j = 0; j < L; j++) //pour tous les liens
                     {
-                        // si lien avec gate a un noeud et Skynet a l'autre   et liens open
-                        if((links[j,0]== gate || links[j, 1] == gate) &&
-                            (links[j, 0] == SI || links[j, 1] == SI) &&  links[j, 2] == 1)
+                        // si 1 noeud = gate et  1 noeud = Skynet    et lien open
+                        if ((links[j, 0] == gate || links[j, 1] == gate) &&
+                            (links[j, 0] == SI || links[j, 1] == SI) && links[j, 2] == 1)
                         {
-                            if (!hasClosed) {
+                            if (!hasClosed)
+                            {
                                 Console.WriteLine(links[j, 0] + " " + links[j, 1]);
                                 links[j, 2] = 0;
                                 hasClosed = true;
@@ -63,22 +71,68 @@ namespace IAGames
                     }
                 }
 
-                if (!hasClosed) //on prend nimporte quel lien a closer /*mais lié a une autre gate*/
+                //Sinon chercher le lien le plus loin et fermer le n-1.
+                if (!hasClosed)
                 {
-                 
+                    LiensDe(SI);
+                    var res = lstNoeuds[lstNoeuds.Count - 1];
+                    Console.WriteLine(res.Item1 + " " + res.Item2);
+                }
+
+
+               /* if (!hasClosed) //NE DEVRAIT PAS PASSER on prend nimporte quel lien a closer  ,mais lié a une autre gate
+                {
                     for (int j = 0; j < L; j++)
                     {
                         // si lien open 
-                        if (links[j, 2] == 1 && !hasClosed) {
+                        if (links[j, 2] == 1 && !hasClosed)
+                        {
                             Console.WriteLine(links[j, 0] + " " + links[j, 1]);
                             links[j, 2] = 0;
                             hasClosed = true;
-                        break;
+                            break;
                         }
                     }
-
-                }
+                }*/
             }//END WHILE
         }
+
+        static void LiensDe(int noeudP)
+        {
+            //si noeud est une gate
+            for (int i = 0; i < E; i++)
+            {
+                if (noeudP == gates[i])
+                {
+                    Console.Error.WriteLine("gate atteinte" + noeudP);
+                    return;
+                }
+            }
+
+            for (int j = 0; j < L; j++)
+            {
+               if( !lstNoeuds.Contains(new Tuple<int, int>(links[j, 1], noeudP)) && !lstNoeuds.Contains(new Tuple<int, int>(noeudP, links[j, 1]))) { 
+                    if (links[j, 0] == noeudP && links[j, 2] == 1)
+                    {
+                        Console.Error.WriteLine("add"+ links[j, 1] + " "+ noeudP);
+
+                        lstNoeuds.Add(new Tuple<int, int>(links[j, 1], noeudP)); //noeud, noeud parent
+
+                        LiensDe(links[j, 1]);
+                        break;
+                    }
+                    if (links[j, 1] == noeudP && links[j, 2] == 1)
+                    {
+                        Console.Error.WriteLine("add " + links[j, 0] + "  P" + noeudP);
+
+                        lstNoeuds.Add(new Tuple<int, int>(links[j, 0], noeudP)); //noeud, noeud parent
+
+                        LiensDe(links[j, 0]);
+                        break;
+                    }
+               }
+            }
+        }
+
     }
 }
